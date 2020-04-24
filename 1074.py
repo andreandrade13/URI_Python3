0ad859278ee1f1bb12fad97fc775fc33b38b08dc
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-04-24 09:08:14 -0300

    Update 1074.py

--- 1074.py
+++ 1074.py
@@ -1,12 +1,15 @@
-x = int(input())
-
-if(x == 0):
-    print("NULL")
-elif(x % 2 == 0 and x > 0):
-    print("ODD POSITIVE")
-elif(x % 2 == 0 and x < 0):
-    print("ODD NEGATIVE")
-elif(x % 2 != 0 and x > 0):
-    print("EVEN POSITIVE")
-elif(x % 2 != 0 and x < 0):
-    print("ODD NEGATIVE")
+l=[]
+a=int(input())
+for n in range(1,(a+1)):
+    l.append(int(input()))
+for n in l:
+    if (n%2!=0)and (n<0):
+        print("ODD NEGATIVE")
+    if(n==0):
+        print("NULL")
+    if(n%2!=0)and(n>0):
+        print("ODD POSITIVE")
+    if(n%2==0)and(n<0):
+        print("EVEN NEGATIVE")
+    if(n%2==0)and(n>0):
+        print("EVEN POSITIVE")

c3a2e1a5ea30adcfd520468ece099122385a9611
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-04-24 09:09:35 -0300

    Update 1061.py

--- 1061.py
+++ 1061.py
@@ -1,36 +1,31 @@
-valor = input().split()
-d1 = int(valor[1])
-
-valores = input().split(" : ")
-h1,m1,s1 = list(map(int,valores))
-
-valor = input().split()
-d2 = int(valor[1])
-
-valores = input().split(" : ")
-h2,m2,s2 = list(map(int,valores))
-
-d = d2 - d1
-
-h = h2 - h1
-if(h < 0):
-    h = 24 + h
-    d = d - 1
-
-m = m2 - m1
-if(m < 0):
-    m = 60 + m
-    h = h -1
-    
-s = s2 - s1
-if(s < 0):
-    s = 60 + s
-    m = m - 1
-    
-if(d <= 0):
-    d = 0
-    
-print("%d dia(s)" %d)
-print("%d hora(s)" %h)
-print("%d minutos(s)" %m)
-print("%d segundos(s)" %s)
+di = input().split(" ")
+w = int(di[1])
+hi = input().split(" ")
+x = int(hi[0])
+y = int(hi[2])
+z = int(hi[4])
+df = input().split(" ")
+w1 = int(df[1])
+hf = input().split(" ")
+x1 = int(hf[0])
+y1 = int(hf[2])
+z1 = int(hf[4])
+dia = w1 - w 
+hora = x1 - x 
+if(hora < 0):
+	hora = 24 + hora
+	dia = dia - 1
+minuto = y1 - y
+if(minuto < 0):
+	minuto = 60 + minuto
+	hora = hora - 1
+segundos = z1 - z
+if(segundos < 0):
+	segundos = 60 + segundos
+	minuto = minuto - 1
+if(dia <= 0):
+	dia = 0
+print("%d dia(s)" %dia)
+print("%d hora(s)" %hora)
+print("%d minuto(s)" %minuto)
+print("%d segundo(s)" %segundos)

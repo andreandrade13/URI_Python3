0ae81a2e6b4b26c1ac6869bfc1fbda40b8467c29
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-04-29 08:56:39 -0300

    Update 1173.py

--- 1173.py
+++ 1173.py
@@ -1,7 +1,12 @@
-n = [0,0,0,0,0,0,0,0,0,0]
-v = int(input())
+V = int(input())
+N = [1,2,4,8,16,32,64, 128, 256, 512]
 
-for i in range(len(n)):
-    n[i] = v
-    v = v * 2
-    print('n[{}] = {}'.format(i, n[i]))
+for i in range(len(N)):
+  N[i] = N[i]*V 
+  
+      
+  
+  
+  
+for i in range(len(N)):
+ print("N[" + str(i) + "] = " + str(N[i]))

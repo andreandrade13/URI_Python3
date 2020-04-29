083391ada59b659b8b8f2538b74ae43440012c27
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-04-29 08:41:25 -0300

    Update 1184.py

--- 1184.py
+++ 1184.py
@@ -1,33 +1,15 @@
-o = input()
-m = []
+operador = input()
+soma = 0
+cont = 0
 
 for i in range(12):
-    m.append([])
-    
-    for j in range(12):
-        x = float(input())
-        m[i].append(x)
+  for j in range(12):
+    valor = float(input())
+    if i > j:
+      soma += valor
+      cont += 1
 
-if(o == 'S'):
-    s = 0
-    c = 11
-    for i in range(11,0,-1):
-        for j in range(0,c):
-            #print('m[{}][{}]'.format(i,j))
-            s = s + m[i][j]
-        c = c - 1
-    print('{:.1f}'.format(s))  
-    
-    
-if(o == 'M'):
-    s = 0
-    c = 11
-    for i in range(11,0,-1):
-        for j in range(0,c):
-            #print('m[{}][{}]'.format(i,j))
-            s = s + m[i][j]
-            c2 = c2 + 1
-        c = c - 1
-        
-    m = s / (c2)
-    print('{:.1f}'.format(s))  
+if operador == "S":
+  print("%.1f" %soma)
+else:
+  print("%.1f" %(soma / cont))

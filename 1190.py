16d7179abadb33bf16cea036903bd0a30add0344
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-04-29 08:42:59 -0300

    Update 1190.py

--- 1190.py
+++ 1190.py
@@ -1,15 +1,15 @@
-operador= input()
+operador = input()
 soma = 0
 cont = 0
 
 for i in range(12):
-    for j in range(12):
-        valor = float(input())
-        if(i+j>=12 and j-1>=1):
-            soma += valor
-            cont += 1
-            
-if(operador == 'S'):
-    print('%.1f' %soma)
+  for j in range(12):
+    valor = float(input())
+    if(i + j >= 12 and j - i >= 1):
+      soma += valor
+      cont += 1
+
+if operador == "S":
+  print("%.1f" %soma)
 else:
-    print('%.1f' %(soma / cont))
+  print("%.1f" %(soma / cont))

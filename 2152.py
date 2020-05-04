d3ad3f313428753827ad46766eaf07dd31045c69
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-05-04 08:29:42 -0300

    Create 2152.py

---
+++ 2152.py
@@ -0,0 +1,12 @@
+c = int(input())
+
+for i in range(c):
+    x = input()
+    x = x.split(' ')
+
+    print('%02d:%02d - ' %(int(x[0]), int(x[1])), end='')
+
+    if x[2] == '1':
+        print('A porta abriu!')
+    elif x[2] == '0':
+        print('A porta fechou!')

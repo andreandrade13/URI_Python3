f388661ed89f848ec638c979e185d52624aeafa5
andreandrade13 <43358672+andreandrade13@users.noreply.github.com>
2020-05-04 09:39:41 -0300

    Create 2582.py

---
+++ 2582.py
@@ -0,0 +1,29 @@
+c = int(input())
+
+for i in range(c):
+    x = input()
+    x = x.split(' ')
+    a = int(x[0]) + int(x[1])
+
+    if a == 0:
+        print('PROXYCITY')
+    elif a == 1:
+        print('P.Y.N.G.')
+    elif a == 2:
+        print('DNSUEY!')
+    elif a == 3:
+        print('SERVERS')
+    elif a == 4:
+        print('HOST!')
+    elif a == 5:
+        print('CRIPTONIZE')
+    elif a == 6:
+        print('OFFLINE DAY')
+    elif a == 7:
+        print('SALT')
+    elif a == 8:
+        print('ANSWER!')
+    elif a == 9:
+        print('RAR?')
+    elif a == 10:
+        print('WIFI ANTENNAS')
